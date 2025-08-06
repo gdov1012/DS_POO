d1a162a9aa23edd2681a67ff8cb361347b5a29d3
gdov1012 <valerogiovanna7@gmail.com>
2025-08-05 22:02:11 -0300

    Add files via upload

---
+++ Aula01ClassesObjetos/Pessoa.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Pessoa {
+   
+    //declaração de atributos
+    String nome;
+    int idade;
+    String RG;
+    
+    // declaração de métodos
+    public void exibirInfos(){
+        System.out.println("Nome " +nome);
+        System.out.println("Idade " +idade);
+        System.out.println("Rg " +RG);
+          
+    }
+    
+}
